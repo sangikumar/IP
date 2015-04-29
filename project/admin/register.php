f4d00dbf05680274cbd2ca2d09d7a44bf34cd98a
whitebox <whiteboxqa@gmail.com>
2015-04-29 16:21:00 -0700

    bootstrap

--- project/admin/register.php
+++ project/admin/register.php
@@ -1,152 +1,152 @@
 
-<!DOCTYPE html>
-<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
-<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
-<!--[if !IE]><!-->
-<html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html"
-      xmlns="http://www.w3.org/1999/html"> <!--<![endif]-->
-<head>
-    <title>UI/UX Training - INNOVAPATH, INC.</title>
-    <!-- Meta -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="UI/UX Training">
-    <meta name="author" content="Innovapath, Inc.">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <link rel="shortcut icon" href="favicon.ico">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
-    <!-- Global CSS -->
-    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
-    <!-- Plugins CSS -->
-    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" href="assets/plugins/flexslider/flexslider.css">
-    <link rel="stylesheet" href="assets/plugins/pretty-photo/css/prettyPhoto.css">
-    <!-- Theme CSS -->
-    <link id="theme-style" rel="stylesheet" href="assets/css/styles.css">
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
 
-<body>
-<div class="wrapper">
-    <!-- ******HEADER****** -->
-    <header class="header">
-        <div class="hidden-xs top-bar">
-            <div class="container">
-
-            </div>
-        </div>
-        <!--//to-bar-->
-        <div class="header-main container">
-            <h1 class="logo col-md-4 col-sm-4">
-                <a href="index.html"><img id="logo" src="images/logo.png" alt="Logo"></a>
-            </h1><!--//logo-->
-
-        </div>
-        <!--//header-main-->
-    </header>
-    <!-- ******NAV****** -->
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <!--//nav-toggle-->
-            </div>
-            <!--//navbar-header-->
-            <div class="navbar-collapse collapse" id="navbar-collapse">
-
-                <class="nav nav-pills nav-stacked">
-
-                <li class="active"><a href="index.html">Home</a></li>
-                <li class="active"><a href="Login.html">Login</a></li>
-                <li class="active"><a href="Register.html">Register</a></li>
-
-
-                </li>
-
-                </ul>
-                <!--//nav-->
-            </div>
-            <!--//navabr-collapse-->
-        </div>
-        <!--//container-->
-    </nav>
-    <!--//main-nav-->
-    <!-- ******CONTENT****** -->
-    <div class="content container">
-        <div class="page-wrapper">
-
-            <div class="page-content">
-                <div class="row">
-                    <article class="contact-form col-sm-6 page-row">
-                        <h3 class="title">INNOVAPATH, INC. - Avata</h3>
-                        <p>Register</p>
-                        <form>
-                            <div class="form-group name">
-                                <label>Name</label>
-                                <input type="text" class="form-control" placeholder="Enter your name">
-                            </div><!--//form-group-->
-                            <div class="form-group name">
-                                <label>Display Name</label>
-                                <input type="text" class="form-control" placeholder="Enter your display name">
-                            </div><!--//form-group-->
-                            <div class="form-group name">
-                                <label>password</label>
-                                <input type="text" class="form-control" placeholder="Enter your password">
-                            </div><!--//form-group-->
-                            <div class="form-group name">
-                                <label>confirm</label>
-                                <input type="text" class="form-control" placeholder="Enter your confirm">
-                            </div><!--//form-group-->
-                            <div class="form-group email">
-                                <label for="email">Email<span class="required">*</span></label>
-                                <input id="email" type="email" class="form-control"  rows="3" placeholder="Enter your email">
-                            </div><!--//form-group-->
-                            <div class="form-group phone">
-                                <label for="phone">Phone</label>
-                                <input id="phone" type="tel" class="form-control" placeholder="Enter your contact number">
-                            </div><!--//form-group-->
-                            <label for="phone">security</label>
-
-                </div><!--//form-group-->
-                <label for="phone">Enter security</label>
-                <input id="phone" type="tel" class="form-control" placeholder="Enter your contact code">
-            </div><!--//form-group-->
-            </br>
-
-            <button type="button" class="btn btn-danger">Register</button>
-            </form>
-            </article><!--//contact-form-->
-
-        </div><!--//content-->
-    </div><!--//wrapper-->
-
-    <!-- ******FOOTER****** -->
-    <!-- Javascript -->
-    <script type="text/javascript" src="assets/plugins/jquery-1.10.2.min.js"></script>
-    <script type="text/javascript" src="assets/plugins/jquery-migrate-1.2.1.min.js"></script>
-    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="assets/plugins/bootstrap-hover-dropdown.min.js"></script>
-    <script type="text/javascript" src="assets/plugins/back-to-top.js"></script>
-    <script type="text/javascript" src="assets/plugins/jquery-placeholder/jquery.placeholder.js"></script>
-    <script type="text/javascript" src="assets/plugins/pretty-photo/js/jquery.prettyPhoto.js"></script>
-    <script type="text/javascript" src="assets/plugins/flexslider/jquery.flexslider-min.js"></script>
-    <script type="text/javascript" src="assets/plugins/jflickrfeed/jflickrfeed.min.js"></script>
-    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
     <script type="text/javascript" src="assets/plugins/gmaps/gmaps.js"></script>
     <script type="text/javascript" src="assets/js/map.js"></script>
     <script type="text/javascript" src="assets/js/main.js"></script>
 
+    <!DOCTYPE html>
+    <!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+    <!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+    <!--[if !IE]><!-->
+    <html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html"
+          xmlns="http://www.w3.org/1999/html"> <!--<![endif]-->
+    <head>
+        <title>UI/UX Training - INNOVAPATH, INC.</title>
+        <!-- Meta -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="UI/UX Training">
+        <meta name="author" content="Innovapath, Inc.">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <link rel="shortcut icon" href="favicon.ico">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+        <!-- Global CSS -->
+        <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+        <!-- Plugins CSS -->
+        <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
+        <link rel="stylesheet" href="assets/plugins/flexslider/flexslider.css">
+        <link rel="stylesheet" href="assets/plugins/pretty-photo/css/prettyPhoto.css">
+        <!-- Theme CSS -->
+        <link id="theme-style" rel="stylesheet" href="assets/css/styles.css">
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
 
+    <body>
+    <div class="wrapper">
+        <!-- ******HEADER****** -->
+        <header class="header">
+            <div class="hidden-xs top-bar">
+                <div class="container">
+
+                </div>
+            </div>
+            <!--//to-bar-->
+            <div class="header-main container">
+                <h1 class="logo col-md-4 col-sm-4">
+                    <a href="index.html"><img id="logo" src="images/logo.png" alt="Logo"></a>
+                </h1><!--//logo-->
+
+            </div>
+            <!--//header-main-->
+        </header>
+        <!-- ******NAV****** -->
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!--//nav-toggle-->
+                </div>
+                <!--//navbar-header-->
+                <div class="navbar-collapse collapse" id="navbar-collapse">
+
+                    <class="nav nav-pills nav-stacked">
+
+                    <li class="active"><a href="index.html">Home</a></li>
+                    <li class="active"><a href="Login.html">Login</a></li>
+                    <li class="active"><a href="Register.html">Register</a></li>
+
+
+                    </li>
+
+                    </ul>
+                    <!--//nav-->
+                </div>
+                <!--//navabr-collapse-->
+            </div>
+            <!--//container-->
+        </nav>
+        <!--//main-nav-->
+        <!-- ******CONTENT****** -->
+        <div class="content container">
+            <div class="page-wrapper">
+
+                <div class="page-content">
+                    <div class="row">
+                        <article class="contact-form col-sm-6 page-row">
+                            <h3 class="title">INNOVAPATH, INC. - Avata</h3>
+                            <p>Register</p>
+                            <form>
+                                <div class="form-group name">
+                                    <label>Name</label>
+                                    <input type="text" class="form-control" placeholder="Enter your name">
+                                </div><!--//form-group-->
+                                <div class="form-group name">
+                                    <label>Display Name</label>
+                                    <input type="text" class="form-control" placeholder="Enter your display name">
+                                </div><!--//form-group-->
+                                <div class="form-group name">
+                                    <label>password</label>
+                                    <input type="text" class="form-control" placeholder="Enter your password">
+                                </div><!--//form-group-->
+                                <div class="form-group name">
+                                    <label>confirm</label>
+                                    <input type="text" class="form-control" placeholder="Enter your confirm">
+                                </div><!--//form-group-->
+                                <div class="form-group email">
+                                    <label for="email">Email<span class="required">*</span></label>
+                                    <input id="email" type="email" class="form-control"  rows="3" placeholder="Enter your email">
+                                </div><!--//form-group-->
+                                <div class="form-group phone">
+                                    <label for="phone">Phone</label>
+                                    <input id="phone" type="tel" class="form-control" placeholder="Enter your contact number">
+                                </div><!--//form-group-->
+                                <label for="phone">security</label>
+
+                    </div><!--//form-group-->
+                    <label for="phone">Enter security</label>
+                    <input id="phone" type="tel" class="form-control" placeholder="Enter your contact code">
+                </div><!--//form-group-->
+                </br>
+
+                <button type="button" class="btn btn-danger">Register</button>
+                </form>
+                </article><!--//contact-form-->
+
+            </div><!--//content-->
+        </div><!--//wrapper-->
+
+        <!-- ******FOOTER****** -->
+        <!-- Javascript -->
+        <script type="text/javascript" src="assets/plugins/jquery-1.10.2.min.js"></script>
+        <script type="text/javascript" src="assets/plugins/jquery-migrate-1.2.1.min.js"></script>
+        <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="assets/plugins/bootstrap-hover-dropdown.min.js"></script>
+        <script type="text/javascript" src="assets/plugins/back-to-top.js"></script>
+        <script type="text/javascript" src="assets/plugins/jquery-placeholder/jquery.placeholder.js"></script>
+        <script type="text/javascript" src="assets/plugins/pretty-photo/js/jquery.prettyPhoto.js"></script>
+        <script type="text/javascript" src="assets/plugins/flexslider/jquery.flexslider-min.js"></script>
+        <script type="text/javascript" src="assets/plugins/jflickrfeed/jflickrfeed.min.js"></script>
+        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
 </body>
 </html>
 
